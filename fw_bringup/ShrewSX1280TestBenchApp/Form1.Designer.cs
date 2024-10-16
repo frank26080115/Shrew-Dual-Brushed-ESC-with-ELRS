81bc8f1314e23a4810e069310dcc643be426227a
frank26080115 <frank.zhao.main@gmail.com>
2024-10-15 23:41:32 -0700

    starting the fcc test gui for sx1280

---
+++ fw_bringup/ShrewSX1280TestBenchApp/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ShrewSX1280TestBenchApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dropSerialPorts = new System.Windows.Forms.ComboBox();
+            this.btnLoadFirmware = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.dropSerialPorts);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(630, 58);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial Port";
+            // 
+            // dropSerialPorts
+            // 
+            this.dropSerialPorts.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dropSerialPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dropSerialPorts.FormattingEnabled = true;
+            this.dropSerialPorts.Location = new System.Drawing.Point(6, 19);
+            this.dropSerialPorts.Name = "dropSerialPorts";
+            this.dropSerialPorts.Size = new System.Drawing.Size(618, 21);
+            this.dropSerialPorts.TabIndex = 0;
+            this.dropSerialPorts.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dropSerialPorts_MouseDoubleClick);
+            // 
+            // btnLoadFirmware
+            // 
+            this.btnLoadFirmware.Location = new System.Drawing.Point(12, 76);
+            this.btnLoadFirmware.Name = "btnLoadFirmware";
+            this.btnLoadFirmware.Size = new System.Drawing.Size(116, 40);
+            this.btnLoadFirmware.TabIndex = 1;
+            this.btnLoadFirmware.Text = "Load Firmware";
+            this.btnLoadFirmware.UseVisualStyleBackColor = true;
+            this.btnLoadFirmware.Click += new System.EventHandler(this.btnLoadFirmware_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 450);
+            this.Controls.Add(this.btnLoadFirmware);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox dropSerialPorts;
+        private System.Windows.Forms.Button btnLoadFirmware;
+    }
+}
+
---
+++ fw_bringup/ShrewSX1280TestBenchApp/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Management;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ShrewSX1280TestBenchApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = Application.ProductName;
+            PopulateSerialPorts();
+        }
+
+        private void PopulateSerialPorts()
+        {
+            string oldItem = string.Empty;
+            if (dropSerialPorts.Items.Count > 0)
+            {
+                oldItem = (string)dropSerialPorts.SelectedItem;
+            }
+            dropSerialPorts.Items.Clear();
+
+            foreach (string port in SerialPort.GetPortNames())
+            {
+                string portInfo = GetPortInformation(port);
+                dropSerialPorts.Items.Add(portInfo);
+            }
+            if (oldItem.Length > 0)
+            {
+                SelectClosestMatch(dropSerialPorts, oldItem);
+            }
+            else if (dropSerialPorts.Items.Count > 0)
+            {
+                dropSerialPorts.SelectedIndex = 0;
+            }
+        }
+
+        private void SelectClosestMatch(ComboBox comboBox, string searchText)
+        {
+            // Initialize variables to store the closest match and minimum distance
+            string closestMatch = null;
+            int minDistance = int.MaxValue;
+
+            // Iterate through the ComboBox items
+            foreach (string item in comboBox.Items)
+            {
+                // Calculate the Levenshtein distance
+                int distance = LevenshteinDistance(item, searchText);
+
+                // Update the closest match if a closer one is found
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestMatch = item;
+                }
+            }
+
+            // Select the closest match in the ComboBox
+            if (closestMatch != null)
+            {
+                comboBox.SelectedItem = closestMatch;
+            }
+
+        }
+        private string GetPortInformation(string portName)
+        {
+            string info = portName;
+            try
+            {
+                using (ManagementObjectSearcher searcher = new ManagementObjectSearcher($"SELECT * FROM Win32_PnPEntity WHERE Caption LIKE '%({portName}%'"))
+                {
+                    foreach (ManagementObject queryObj in searcher.Get())
+                    {
+                        string t = $" - {queryObj["Caption"]} ({queryObj["Description"]})";
+                        string pattern = $@"{Regex.Escape(portName)}([^a-zA-Z0-9]*|$)";
+                        if (Regex.IsMatch(t, pattern))
+                        {
+                            info += t;
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                info += " - Info not available";
+            }
+
+            return info;
+        }
+
+
+        private void dropSerialPorts_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            PopulateSerialPorts();
+        }
+
+        private int LevenshteinDistance(string s1, string s2)
+        {
+            int[,] d = new int[s1.Length + 1, s2.Length + 1];
+
+            for (int i = 0; i <= s1.Length; i++)
+                d[i, 0] = i;
+
+            for (int j = 0; j <= s2.Length; j++)
+                d[0, j] = j;
+
+            for (int i = 1; i <= s1.Length; i++)
+            {
+                for (int j = 1; j <= s2.Length; j++)
+                {
+                    int cost = (s2[j - 1] == s1[i - 1]) ? 0 : 1;
+                    d[i, j] = Math.Min(Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1), d[i - 1, j - 1] + cost);
+                }
+            }
+
+            return d[s1.Length, s2.Length];
+        }
+
+        private void btnLoadFirmware_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
